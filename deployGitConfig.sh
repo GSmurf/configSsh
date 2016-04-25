3b6129e6130dc50a137892fbabedbc5db5bad3b0
fsqdf <qsdf>
2016-04-25 14:53:51 +0200

    Non fonctionnel : Factorisation du changement d'information

--- deployGitConfig.sh
+++ deployGitConfig.sh
@@ -6,19 +6,32 @@ $(cat ~/.gitconfig|grep 'name ='|awk '/'^$'/ { print $3" "$4 }')
 # recupere le nom de l'utilisateur git en cours
 mailGit=$(cat ~/.gitconfig|grep 'email ='|awk '/'^$'/ { print $3 }')
 
+
+function changeInfosUserGit
+{
+    echo "nom : $1, mail : $2"
+
+    cp .gitconfig .gitconfigNew
+    sed -i "s/#name = REALNAME/name = $1/g" .gitconfigNew 
+    sed -i "s/#email = USERNAME/email = $2/g" .gitconfigNew 
+    mv .gitconfigNew ~/.gitconfig
+}
+
+
 echo "Les informations GIT récupérées sont : $nomUserGit ($mailGit), vous confirmez (y/n) ?"
 read ok
 
 case $ok in
     "y" )
         echo "Remplacement des informations générique par les données : $nomUserGit ($mailGit)"
-        cp .gitconfig .gitconfigNew
-        sed -i "s/#name = REALNAME/name = ${nomUserGit}/g" .gitconfigNew 
-        sed -i "s/#email = USERNAME/email = ${mailGit}/g" .gitconfigNew 
-        mv .gitconfigNew ~/.gitconfig
+        changeInfosUserGit $nomUserGit $mailGit
     ;;
     "n" )
-        echo "non"
+        echo "saisir le Nom user ex, Stéphane S. :"
+        read nomUserGit
+        echo "saisir l'email ex, test@gmail.com :"
+        read mailGit
+        changeInfosUserGit $nomUserGit $mailGit
     ;;
     *)
         echo "rien"
